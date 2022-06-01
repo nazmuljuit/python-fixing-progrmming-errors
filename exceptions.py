adbb9cc6ec2db5155577fd0e209c64bb8ee848c2
Nazmul Hossain <nazmuljuit@gmail.com>
2022-06-01 19:41:19 +0600

    first command of errors

---
+++ exceptions.py
@@ -0,0 +1,10 @@
+a = 1
+b = "2"
+print(int(2.6))
+#type error here
+#print(a+b)
+print(a+float(b))
+print(str(a)+b)
+#name error
+#print(c)
+#print(5/0)
---
+++ type_erros.py
@@ -0,0 +1,7 @@
+print(1)
+int(9)
+#int 9
+print(2)
+#print 3
+#a =[1,2,3)
+a =[1,2,3]
